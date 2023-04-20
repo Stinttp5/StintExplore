8d4b0a39feb76b81803b5ec407f618b7a5d30e1a
Nicholas Jennings <nicholasjennings@berkeley.edu>
2023-04-19 18:39:51 -0700

    add cat coding webview tutorial

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,56 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('stintexplore.cat', () => {
+		  const panel = vscode.window.createWebviewPanel(
+			'catCoding',
+			'Cat Coding',
+			vscode.ViewColumn.One,
+			{}
+		  );
+	
+		  let iteration = 0;
+		  const updateWebview = () => {
+			const cat = iteration++ % 2 ? 'Compiling Cat' : 'Coding Cat';
+			panel.title = cat;
+			panel.webview.html = getWebviewContent(cat);
+		  };
+	
+		  // Set initial content
+		  updateWebview();
+	
+		  // And schedule updates to the content every second
+		  const interval = setInterval(updateWebview, 1000);
+
+		  panel.onDidDispose(
+			() => {
+			  // When the panel is closed, cancel any future updates to the webview content
+			  clearInterval(interval);
+			},
+			null,
+			context.subscriptions
+		  );
+		})
+	  );
 }
+const cats = {
+	'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+	'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif'
+  };
+function getWebviewContent(cat: keyof typeof cats) {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <img src="${cats[cat]}" width="300" />
+  </body>
+  </html>`;
+  }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
